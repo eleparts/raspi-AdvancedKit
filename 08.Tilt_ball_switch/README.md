179749193a24b017adb8c0c058a0d1a6f65fc873
eleparts <nyk.yeon@eleparts.co.kr>
2020-09-29 08:19:24 +0900

    예제 업데이트

---
+++ 08.Tilt_ball_switch/README.md
@@ -0,0 +1,23 @@
+# 라즈베리파이 고급 키트 - 볼스위치
+
+블로그 바로가기 : [라즈베리파이 고급 키트 - 볼스위치](https://blog.naver.com/elepartsblog/221515617750)  
+
+## 회로 연결  
+
+자세한 사항은 블로그 설명을 참고해 주세요.  
+
+![tilt_ball_switch](https://blogfiles.pstatic.net/MjAxOTA0MTdfMjgg/MDAxNTU1NDYwMzA3OTA4.yyTMQM7t_UT04oPm-El-NSyphGlNHOR7wZ3JyrvEhNYg.JeO2Tt0isf3CrLZbItkaSdm8PyrkJ3mMkOFxc87ZRyYg.PNG.elepartsblog/2.PNG?type=w2)  
+
+## 예제 실행  
+
+### GUI 환경에서 실행  
+
+Thonny 편집기로 .py 파일을 열고 실행(Run)버튼으로 프로그램을 실행해 줍니다.  
+
+### CLI (터미널)환경에서 실행  
+
+터미널 창에서 파일이 있는 디렉토리로 이동, 아래의 명령어로 실행해 줍니다.  
+
+```bash
+sudo python3 tilt_ball_switch.py  
+```
---
+++ 08.Tilt_ball_switch/tilt_ball_switch.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+
+b_sw=16
+led_r=24
+led_b=23
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(b_sw,GPIO.IN,GPIO.PUD_DOWN)  # 내부 풀 다운 사용
+GPIO.setup(led_r,GPIO.OUT)
+GPIO.setup(led_b,GPIO.OUT)
+
+def turn_ledB(self):                    # 함수 만들기 / (self) = 받아오는 인자가 없을 때 void의 의미
+  GPIO.output(led_b,GPIO.HIGH)
+  time.sleep(1)
+  GPIO.output(led_b,GPIO.LOW)
+
+GPIO.add_event_detect(b_sw,GPIO.FALLING,turn_ledB) # 인터럽트 사용
+
+'''
+GPIO.add_event_detect(channel,edge,callback=none,bouncetime=none)
+channel : 입력으로 설정된 GPIO 번호
+edge : 이벤트 실행 신호 (RISING,FALLING,BOTH)
+callback : 이벤트 발생 시 호출할 함수
+bouncetime : 이 시간 동안 한 번의 이벤트만 실행
+'''
+
+try:
+  while 1:
+    GPIO.output(led_r,GPIO.HIGH)
+    time.sleep(0.5)
+
+    GPIO.output(led_r,GPIO.LOW)
+    time.sleep(0.5)
+
+except KeyboardInterrupt:         # ctrl+c 종료
+  GPIO.cleanup()
+
+ 

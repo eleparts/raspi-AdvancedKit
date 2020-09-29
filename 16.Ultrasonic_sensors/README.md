85a5f6738b02bc606a9066b8ca70209e730381a4
eleparts <nyk.yeon@eleparts.co.kr>
2020-09-29 15:06:28 +0900

    예제 업데이트

---
+++ 16.Ultrasonic_sensors/README.md
@@ -0,0 +1,23 @@
+# 라즈베리파이 고급 키트 - 초음파센서
+
+블로그 바로가기 : [라즈베리파이 고급 키트 - 초음파센서](https://blog.naver.com/elepartsblog/221533957300)  
+
+## 회로 연결  
+
+자세한 사항은 블로그 설명을 참고해 주세요.  
+
+![ultrasonic_sensors](https://blogfiles.pstatic.net/MjAxOTA1MTBfMTIg/MDAxNTU3NDQ3ODI2MDQx.1wmfCX0WNo8d5jHOw36lA2olOvcChRcIGV8WrBsTCdkg.1GIvPybxbjrPdEISB0oNlPQLoCMprvmA_5igxbWgEYYg.PNG.elepartsblog/3.PNG?type=w2)  
+
+## 예제 실행  
+
+### GUI 환경에서 실행  
+
+Thonny 편집기로 .py 파일을 열고 실행(Run)버튼으로 프로그램을 실행해 줍니다.  
+
+### CLI (터미널)환경에서 실행  
+
+터미널 창에서 파일이 있는 디렉토리로 이동, 아래의 명령어로 실행해 줍니다.  
+
+```bash
+sudo python3 ultrasonic_sensors.py  
+```
---
+++ 16.Ultrasonic_sensors/ultrasonic_sensors.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO_TRIGGER  = 23
+GPIO_ECHO     = 24
+
+print("start")
+
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+try:
+  while True:
+
+    # 트리거 핀 high 설정
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # 0.00001초 sleep 후 트리거 핀 low 설정
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # 시작 시간 저장
+    while GPIO.input(GPIO_ECHO) == 0:
+      StartTime = time.time()
+
+    # 마지막 시간 저장
+    while GPIO.input(GPIO_ECHO) == 1:
+      StopTime = time.time()
+
+    # 시작 시간과 마지막 시간의 차이 계산, 저장
+    TimeElapsed = StopTime - StartTime
+
+    # 음속을 곱해 거리 계산, 2로(왕복) 나누기
+    distance = round((TimeElapsed * 34300) / 2, 2)
+
+    # 계산된 거리 출력
+    print ("Distance = " , distance)
+    time.sleep(1)
+
+except KeyboardInterrupt:
+
+  GPIO.cleanup()
+
